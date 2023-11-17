8a7ef06cd711e7bb1304116f0d9ae279bf401a90
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2023-11-17 17:10:41 +0200

    3d task started. destructurization  needs to be done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+// const submitButton = document.querySelector('button');
+
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+populateTextarea();
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInput(event) {
+  const message = event.target.value;
+  //   const data = {
+  //     email: email.value.trim(),
+  //     message: message.value.trim(),
+  //   };
+  localStorage.setItem(FEEDBACK_FORM_KEY, message);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  //   const { email, message } = event.currentTarget.elements;
+  //   console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_KEY);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(FEEDBACK_FORM_KEY);
+  if (savedMessage) {
+    textarea.value = savedMessage;
+  }
+}
